2e48665d382a3406f550c99f930e02d369cd7eb3
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2025-03-08 05:14:26 +0100

    initial commit

---
+++ .projenrc.ts
@@ -0,0 +1,56 @@
+import { awscdk, javascript } from "projen";
+import { IndentStyle } from "projen/lib/javascript/biome/biome-config";
+
+const project = new awscdk.AwsCdkTypeScriptApp({
+  name: "cdk-aws-ecs-otel-collector",
+
+  // Base
+  defaultReleaseBranch: "main",
+  depsUpgradeOptions: { workflow: false },
+  gitignore: ["cdk.context.json"],
+  projenrcTs: true,
+
+  // Toolchain
+  biome: true,
+  biomeOptions: {
+    biomeConfig: {
+      assist: {
+        enabled: true,
+        actions: {
+          source: {
+            organizeImports: {
+              level: "on",
+              options: {
+                identifierOrder: "lexicographic",
+              },
+            },
+          },
+        },
+      },
+      formatter: {
+        enabled: true,
+        indentStyle: IndentStyle.SPACE,
+        indentWidth: 2,
+        lineWidth: 100,
+      },
+      linter: {
+        enabled: true,
+        rules: {
+          recommended: true,
+          suspicious: {
+            noShadowRestrictedNames: "off",
+          },
+        },
+      },
+    },
+  },
+  cdkVersion: "2.229.1",
+  minNodeVersion: "24.11.1",
+  packageManager: javascript.NodePackageManager.PNPM,
+  pnpmVersion: "10",
+
+  // Deps
+  devDeps: ["zod"],
+});
+
+project.synth();
